07622edf498260371f14f3678506bc985d9a046d
Bharat Kashyap <bharatkashyap@outlook.com>
2021-06-25 17:27:57 +0530

    fix: keep only POST route

--- api/src/controllers/donate-device.controller.ts
+++ api/src/controllers/donate-device.controller.ts
@@ -42,16 +42,11 @@ export class DonateDeviceController {
     donateDevice: Omit<DonateDevice, 'id'>,
   ): Promise<DonateDevice> {
     const instanceID = donateDevice.data[0]?.instanceID;
- 
     const trackingKey = instanceID.split(':')?.[1]?.split('-')?.[0].toUpperCase();
-    console.log(trackingKey);
     const data = donateDevice?.data?.[0];
-
     const smsBody = `You have successfully registered for donating your smartphone as part of "Baccho ka Sahara, Phone Humara" campaign. Your tracking ID is ${trackingKey}. You can use this ID to track the status of delivery for your donated device.\n\n- Samagra Shiksha, Himachal Pradesh`;
     const contactNumber = data.contact;
-    const smsDispatchResponse = sendSMS(smsBody, contactNumber)
-
-    //TODO: Log SMS send event
+    const smsDispatchResponse = sendSMS(smsBody, trackingKey, contactNumber)    
 
     data.trackingKey = trackingKey;
     const donateDeviceType = new DonateDeviceType(data);
@@ -61,86 +56,86 @@ export class DonateDeviceController {
     return this.donateDeviceRepository.create(donateDevice);    
   }
 
-  @get('/donate-devices/count')
-  @response(200, {
-    description: 'DonateDevice model count',
-    content: { 'application/json': { schema: CountSchema } },
-  })
-  async count(
-    @param.where(DonateDevice) where?: Where<DonateDevice>,
-  ): Promise<Count> {
-    return this.donateDeviceRepository.count(where);
-  }
+  // @get('/donate-devices/count')
+  // @response(200, {
+  //   description: 'DonateDevice model count',
+  //   content: { 'application/json': { schema: CountSchema } },
+  // })
+  // async count(
+  //   @param.where(DonateDevice) where?: Where<DonateDevice>,
+  // ): Promise<Count> {
+  //   return this.donateDeviceRepository.count(where);
+  // }
 
-  @patch('/donate-devices')
-  @response(200, {
-    description: 'DonateDevice PATCH success count',
-    content: { 'application/json': { schema: CountSchema } },
-  })
-  async updateAll(
-    @requestBody({
-      content: {
-        'application/json': {
-          schema: getModelSchemaRef(DonateDevice, { partial: true }),
-        },
-      },
-    })
-    donateDevice: DonateDevice,
-    @param.where(DonateDevice) where?: Where<DonateDevice>,
-  ): Promise<Count> {
-    return this.donateDeviceRepository.updateAll(donateDevice, where);
-  }
+  // @patch('/donate-devices')
+  // @response(200, {
+  //   description: 'DonateDevice PATCH success count',
+  //   content: { 'application/json': { schema: CountSchema } },
+  // })
+  // async updateAll(
+  //   @requestBody({
+  //     content: {
+  //       'application/json': {
+  //         schema: getModelSchemaRef(DonateDevice, { partial: true }),
+  //       },
+  //     },
+  //   })
+  //   donateDevice: DonateDevice,
+  //   @param.where(DonateDevice) where?: Where<DonateDevice>,
+  // ): Promise<Count> {
+  //   return this.donateDeviceRepository.updateAll(donateDevice, where);
+  // }
 
-  @get('/donate-devices/{id}')
-  @response(200, {
-    description: 'DonateDevice model instance',
-    content: {
-      'application/json': {
-        schema: getModelSchemaRef(DonateDevice, { includeRelations: true }),
-      },
-    },
-  })
-  async findById(
-    @param.path.string('id') id: string,
-    @param.filter(DonateDevice, { exclude: 'where' }) filter?: FilterExcludingWhere<DonateDevice>
-  ): Promise<DonateDevice> {
-    return this.donateDeviceRepository.findById(id, filter);
-  }
+  // @get('/donate-devices/{id}')
+  // @response(200, {
+  //   description: 'DonateDevice model instance',
+  //   content: {
+  //     'application/json': {
+  //       schema: getModelSchemaRef(DonateDevice, { includeRelations: true }),
+  //     },
+  //   },
+  // })
+  // async findById(
+  //   @param.path.string('id') id: string,
+  //   @param.filter(DonateDevice, { exclude: 'where' }) filter?: FilterExcludingWhere<DonateDevice>
+  // ): Promise<DonateDevice> {
+  //   return this.donateDeviceRepository.findById(id, filter);
+  // }
 
-  @patch('/donate-devices/{id}')
-  @response(204, {
-    description: 'DonateDevice PATCH success',
-  })
-  async updateById(
-    @param.path.string('id') id: string,
-    @requestBody({
-      content: {
-        'application/json': {
-          schema: getModelSchemaRef(DonateDevice, { partial: true }),
-        },
-      },
-    })
-    donateDevice: DonateDevice,
-  ): Promise<void> {
-    await this.donateDeviceRepository.updateById(id, donateDevice);
-  }
+  // @patch('/donate-devices/{id}')
+  // @response(204, {
+  //   description: 'DonateDevice PATCH success',
+  // })
+  // async updateById(
+  //   @param.path.string('id') id: string,
+  //   @requestBody({
+  //     content: {
+  //       'application/json': {
+  //         schema: getModelSchemaRef(DonateDevice, { partial: true }),
+  //       },
+  //     },
+  //   })
+  //   donateDevice: DonateDevice,
+  // ): Promise<void> {
+  //   await this.donateDeviceRepository.updateById(id, donateDevice);
+  // }
 
-  @put('/donate-devices/{id}')
-  @response(204, {
-    description: 'DonateDevice PUT success',
-  })
-  async replaceById(
-    @param.path.string('id') id: string,
-    @requestBody() donateDevice: DonateDevice,
-  ): Promise<void> {
-    await this.donateDeviceRepository.replaceById(id, donateDevice);
-  }
+  // @put('/donate-devices/{id}')
+  // @response(204, {
+  //   description: 'DonateDevice PUT success',
+  // })
+  // async replaceById(
+  //   @param.path.string('id') id: string,
+  //   @requestBody() donateDevice: DonateDevice,
+  // ): Promise<void> {
+  //   await this.donateDeviceRepository.replaceById(id, donateDevice);
+  // }
 
-  @del('/donate-devices/{id}')
-  @response(204, {
-    description: 'DonateDevice DELETE success',
-  })
-  async deleteById(@param.path.string('id') id: string): Promise<void> {
-    await this.donateDeviceRepository.deleteById(id);
-  }
+  // @del('/donate-devices/{id}')
+  // @response(204, {
+  //   description: 'DonateDevice DELETE success',
+  // })
+  // async deleteById(@param.path.string('id') id: string): Promise<void> {
+  //   await this.donateDeviceRepository.deleteById(id);
+  // }
 }
