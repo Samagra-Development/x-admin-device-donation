815f8fed8a04bfc566e874ec425fde79d740f90b
Bharat Kashyap <bharatkashyap@outlook.com>
2021-06-25 17:28:40 +0530

    fix: send SMS and log it

--- api/src/utils/sendSMS.ts
+++ api/src/utils/sendSMS.ts
@@ -1,13 +1,15 @@
 import axios from 'axios';
+import sendLog from './adminLogger';
 
-const sendSMS = async (msg:string, contactNumber:string) => {
+const sendSMS = async (msg:string, trackingKey: string, contactNumber:string) => {
+        
     const response = await axios({
         method: 'get',
-        url: `http://enterprise.smsgupshup.com/GatewayAPI/rest?msg=${msg}&v=1.1&userid=${process.env.GUPSHUP_USERNAME}&password=${process.env.GUPSHUP_PASSWORD}&send_to=${contactNumber}&msg_type=text&method=sendMessage&format=JSON`
-    });
-
-    const responseObject = response.data?.response;
+        url: `http://enterprise.smsgupshup.com/GatewayAPI/rest?msg=${msg}&v=1.1&userid=${process.env.GUPSHUP_USERNAME}&password=${process.env.GUPSHUP_PASSWORD}&send_to=${contactNumber}&msg_type=text&method=sendMessage&format=JSON&principalEntityId=${process.env.GUPSHUP_PRINCIPAL_ENTITY_ID}&dltTemplateId=1007434778563689331`
+    });    
+    const responseObject = response.data?.response;       
     if (responseObject?.status === 'success') {
+        sendLog(`*samarth-device*: SMS message successfully sent to _${contactNumber}_ with Tracking ID: *${trackingKey}*`, 'C017PG3BBT2');
         return ({
             statusCode: 200,
             error: null,
@@ -15,6 +17,7 @@ const sendSMS = async (msg:string, contactNumber:string) => {
         })
     }
     else {
+        sendLog(`samarth-device: SMS message sending failed to ${contactNumber}`, 'C017PG3BBT2');
         return ({
             statusCode: 500,
             error: `Error sending SMS - Provider error code ${responseObject.id}`,
