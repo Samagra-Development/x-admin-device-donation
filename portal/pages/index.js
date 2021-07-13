229d14ceb15143e9bf872acc70ba45b50ece6d63
sagar-techjoomla <sagar_t@techjoomla.com>
2021-07-13 19:41:53 +0530

    center aline card fixes #4, #5

--- portal/pages/index.js
+++ portal/pages/index.js
@@ -11,19 +11,21 @@ const Home = () => {
         {config.homepageCards.map((card, index) => {
           return (
             <Link key={index} href={card.target} passHref>
-              <div className="card logo-card">
-                <span
-                  className={`material-icons ${styles.icon} ${
-                    styles[card.colour]
-                  }`}
-                >
-                  {card.icon}
-                </span>
-                <h2>
-                  {" "}
-                  {card.title.en} &#47;
-                  <br /> {card.title.hi} &#10230;
-                </h2>
+              <div className="logo-card">
+                <div className="card">
+                  <span
+                    className={`material-icons ${styles.icon} ${
+                      styles[card.colour]
+                    }`}
+                  >
+                    {card.icon}
+                  </span>
+                  <h2>
+                    {" "}
+                    {card.title.en} &#47;
+                    <br /> {card.title.hi} &#10230;
+                  </h2>
+                </div>
               </div>
             </Link>
           );
